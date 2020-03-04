dd616c37ecff5cb22ecd9fff6bb3add4727c697d
Amin NAIRI <nairi.amin@gmail.com>
2020-03-04 13:41:02 +0100

    added an example request

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,3 +1,30 @@
 <template>
     <h1>Page d'accueil</h1>
 </template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import axios from "axios";
+
+export default {
+    methods: {
+        ...mapGetters([
+            "getToken"
+        ]),
+        ...mapActions([
+            "axiosErrorHandler"
+        ])
+    },
+    created() {
+        console.log(this.getToken());
+
+        axios.get("/api/v1/projects", {
+            headers: {
+                "Authorization": `Bearer ${this.getToken()}`
+            }
+        }).then(response => {
+            console.log(response);
+        }).catch(this.axiosErrorHandler);
+    }
+};
+</script>
