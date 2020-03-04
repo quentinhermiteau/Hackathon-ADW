efa516a2aba3b4beb13f02b50c8ccff9e2a15aea
Amin NAIRI <nairi.amin@gmail.com>
2020-03-04 13:42:05 +0100

    added the error handling for axios

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,20 +1,64 @@
 import Vue from "vue";
 import VueX, { Store } from "vuex";
+import M from "materialize-css";
+import router from "./router.js";
+
 
 Vue.use(VueX);
 
 export default new Store({
     state: {
-        token: null
+        token: window.localStorage.getItem("token")
     },
     mutations: {
         SET_TOKEN(state, token) {
             state.token = token;
+
+            window.localStorage.setItem("token", token);
         }
     },
     actions: {
         setToken(context, token) {
             context.commit("SET_TOKEN", token);
+        },
+        axiosErrorHandler(context, error) {
+            console.log("ERROR HANDLER");
+
+            if (error.response.status === 422) {
+                M.toast({
+                    html: Object.values(error.response.data.errors)[0][0],
+                    classes: "red"
+                });
+                
+                return;
+            }
+
+            if (error.response.status === 498) {
+                M.toast({
+                    html: "Session expirée",
+                    classes: "red"
+                });
+
+                context.commit("SET_TOKEN", null);
+
+                router.push("/connexion");
+                
+                return;
+            }
+
+            if (error.response.status === 401) {
+                M.toast({
+                    html: "Identifiants invalides",
+                    classes: "red"
+                });
+
+                return;
+            }
+
+            M.toast({
+                html: "Erreur innatendue, merci de réessayer plus tard",
+                classes: "red"
+            });
         }
     },
     getters: {
