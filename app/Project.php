145653e5319a1547b87614d7d36d4471584c59e6
Amin NAIRI <nairi.amin@gmail.com>
2020-03-04 22:17:09 +0100

    fixed missing referent_id in fillable values & relationship for referent

--- app/Project.php
+++ app/Project.php
@@ -19,7 +19,8 @@ class Project extends Model
         'contact_phone_number',
         'budget_min',
         'budget_max',
-        'user_id'
+        'user_id',
+        'referent_id'
     ];
 
     public function specializations()
@@ -27,8 +28,8 @@ class Project extends Model
         return $this->belongsToMany('App\Specialization');
     }
 
-    public function referent(): User
+    public function referent()
     {
-        return $this->hasOne('App\User', 'referent_id');
+        return $this->belongsTo('App\User', 'referent_id');
     }
 }
