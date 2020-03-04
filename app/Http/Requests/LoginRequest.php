c6c235b2046ec3363e362c929132124a4e494d92
Amin NAIRI <nairi.amin@gmail.com>
2020-03-04 13:38:21 +0100

    removed string from the validation

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -24,8 +24,8 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|string|email',
-            'password' => 'required|string'
+            'email' => 'required|email',
+            'password' => 'required'
         ];
     }
 }
