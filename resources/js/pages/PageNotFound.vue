dcd6cbe6e7688702148a10040cfae36f1d39ca9c
Amin NAIRI <nairi.amin@gmail.com>
2020-03-04 23:54:34 +0100

    better design for the not found page

--- resources/js/pages/PageNotFound.vue
+++ resources/js/pages/PageNotFound.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Page introuvable</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 center">
+                <h1>Page introuvable</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 center">
+                <p class="flow-text">Il semblerait que cette page soit en maintenance ou n'existe plus. Merci de réessayer plus tard.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 center">
+                <router-link to="/">
+                    <button class="btn green waves-effect waves-light">accueil</button>
+                </router-link>
+            </div>
+        </div>
+    </div>
 </template>
