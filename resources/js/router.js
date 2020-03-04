78f8e3c9262e394a03348525f5986e249e3b2dae
Amin NAIRI <nairi.amin@gmail.com>
2020-03-04 23:44:06 +0100

    added project page view

--- resources/js/router.js
+++ resources/js/router.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home.vue";
 import Login from "./pages/Login.vue";
 import PageNotFound from "./pages/PageNotFound.vue";
 import Profil from "./pages/Profil.vue";
+import Project from "./pages/Project.vue";
 import Projects from "./pages/Projects.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
@@ -19,13 +20,14 @@ const router = new VueRouter({
     routes: [
         { path: "*", component: PageNotFound },
         { path: "/", component: Home },
-        { path: "/admin/projet/nouveau", component: AdminProjectNew },
         { path: "/admin/projet/:id", component: AdminProject },
+        { path: "/admin/projet/nouveau", component: AdminProjectNew },
         { path: "/admin/projets", component: AdminProjects },
         { path: "/admin/users", component: AdminUsers },
         { path: "/agents", component: Agents },
         { path: "/connexion", component: Login },
         { path: "/profil/:id", component: Profil },
+        { path: "/projet/:id", component: Project },
         { path: "/projets", component: Projects }
     ]
 });
